9cc72cb1bd285fe663a40eda16745f52a45d56be
Neandher Carlos <neandher89@gmail.com>
2017-02-11 12:47:40 -0200

    ...

--- angular2/src/app/components/base.component.ts
+++ angular2/src/app/components/base.component.ts
@@ -1,8 +1,103 @@
-import {Response} from "@angular/http";
-import {FormGroup} from "@angular/forms";
-export class BaseComponent{
+import {FormGroup, FormControl} from "@angular/forms";
 
-  /*handleResponseError(error: Response, form: FormGroup){
+export class BaseComponent {
 
-  }*/
+  protected form: FormGroup;
+  protected error: string[] = [];
+  protected formErrors: Object;
+
+  protected handleResponseError(responseError: any[]) {
+
+    for (let index in responseError) {
+      var check = 0;
+      for (let item of responseError[index]) {
+        this.error.push(item);
+        var path = this.getPathFormErrorResponse(index, this.form);
+        this.formErrors[(path)].push(item);
+        this.form.get(path).setErrors({"response_error": item});
+        check++;
+      }
+      if (check == 0) {
+        this.handleResponseError(responseError[index]);
+      }
+    }
+  }
+
+  protected getPathFormErrorResponse(field: string, control: any) {
+    var path: string|null = null;
+
+    for (var child in control.controls) {
+      if (control.controls[child] instanceof FormControl) {
+        if (field === child && path == null) {
+          path = this.getPathFormError(control.controls[child]);
+          break;
+        }
+      }
+      else if (path == null) {
+        path = this.getPathFormErrorResponse(field, control.controls[child]);
+      }
+    }
+    return path;
+  }
+
+  protected getPathFormError(control) {
+    var path: string = '';
+    var parent = control.parent;
+
+    while (parent instanceof FormGroup) {
+      Object.keys(parent.controls).forEach((name) => {
+        if (control === parent.controls[name]) {
+          path += name + '.';
+          control = parent;
+        }
+      });
+      parent = parent.parent;
+    }
+
+    path = path.substr(0, (path.length - 1));
+    var dotted = path.indexOf('.');
+
+    if (dotted != -1) {
+      var orderedArray: string[] = path.split('.');
+      orderedArray = orderedArray.reverse();
+      path = '';
+      for (let i = 0; i < orderedArray.length; i++) {
+        path += orderedArray[i] + '.';
+      }
+      path = path.substr(0, (path.length - 1));
+    }
+
+    return path;
+  }
+
+  protected handleError(control: any) {
+
+    const pathFormError = this.getPathFormError(control);
+
+    for (let error in control.errors) {
+      if (this.formErrors[(pathFormError)].indexOf(this.validationMessages[error]) == -1) {
+        this.formErrors[(pathFormError)].push(this.validationMessages[error]);
+      }
+    }
+    for (var child in control.controls) {
+      if (control.controls[child] instanceof FormControl) {
+        this.formErrors[(this.getPathFormError(control.controls[child]))] = [];
+        if (control.controls[child].dirty && !control.controls[child].valid) {
+          this.handleError(control.controls[child]);
+        }
+      }
+      else {
+        this.handleError(control.controls[child]);
+      }
+    }
+  }
+
+  validationMessages: Object = {
+    'response_error': '',
+    'required': 'This field is required.',
+    'minlength': 'This field must be at least 4 characters long.',
+    'maxlength': 'This field cannot be more than 24 characters long.',
+    'passwordsEqual': 'Password mismatch.',
+    'isEmail': 'Email invalid.'
+  };
 }
--- angular2/src/app/components/signup.component.ts
+++ angular2/src/app/components/signup.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormBuilder, Validators, AbstractControl, FormControl} from "@angular/forms";
-import {NGValidators} from 'ng-validators';
+import {Component, OnInit} from "@angular/core";
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
+import {NGValidators} from "ng-validators";
 import {AuthService} from "../services/auth.service";
 import {Router} from "@angular/router";
 import {EqualPasswordsValidator} from "../validators/equalPasswords.validator";
@@ -12,9 +12,16 @@ import {BaseComponent} from "./base.component";
 })
 export class SignUpComponent extends BaseComponent implements OnInit {
 
-  status: string = "";
-  errorAlert: string[] = [];
+  public status: string = "";
+  public error: string[] = [];
   public form: FormGroup;
+  public formErrors: Object = {
+    'firstName': [],
+    'lastName': [],
+    'email': [],
+    'plainPassword.first': [],
+    'plainPassword.second': [],
+  };
 
   constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {
     super();
@@ -22,36 +29,6 @@ export class SignUpComponent extends BaseComponent implements OnInit {
 
   ngOnInit(): void {
     this.buildForm();
-  }
-
-  onSubmit() {
-    this.auth.signupUser(this.form.value).subscribe(
-      response => {
-        this.router.navigate(['/signInUser']);
-      },
-      responseError => {
-        this.errorAlert = [];
-        this.handleResponseError(responseError.json().errors);
-      }
-    );
-  }
-
-  handleResponseError(responseError: any[]) {
-    for (let index in responseError) {
-      var check = 0;
-      for (let item of responseError[index]) {
-        this.errorAlert.push(item);
-        var pathFormError = this.getPathFormError(this.form.get(index));
-        console.log(index);
-        console.log(pathFormError);
-        this.form.get((pathFormError)).setErrors({"response_index": item});
-        check++;
-      }
-      if (check == 0) {
-        this.handleResponseError(responseError[index]);
-      }
-    }
-    this.onValueChanged();
   }
 
   buildForm(): void {
@@ -73,102 +50,19 @@ export class SignUpComponent extends BaseComponent implements OnInit {
     this.onValueChanged();
   }
 
+  onSubmit() {
+    this.auth.signupUser(this.form.value).subscribe(
+      response => {
+        this.router.navigate(['/signInUser']);
+      },
+      responseError => {
+        this.error = [];
+        this.handleResponseError(responseError.json().errors);
+      }
+    );
+  }
+
   onValueChanged() {
     this.handleError(this.form);
   }
-
-  handleError(control: any) {
-
-    const pathFormError = this.getPathFormError(control);
-
-    for (let error in control.errors) {
-      if (this.formErrors[(pathFormError)].indexOf(this.validationMessages[error]) == -1) {
-        this.formErrors[(pathFormError)].push(this.validationMessages[error]);
-      }
-    }
-    for (var child in control.controls) {
-      if (control.controls[child] instanceof FormControl) {
-        this.formErrors[(this.getPathFormError(control.controls[child]))] = [];
-        if (control.controls[child].dirty && !control.controls[child].valid) {
-          this.handleError(control.controls[child]);
-        }
-      }
-      else {
-        this.handleError(control.controls[child]);
-      }
-    }
-  }
-
-  getPathFormError(control) {
-    var path: string = '';
-    var parent = control.parent;
-
-    while (parent instanceof FormGroup) {
-      Object.keys(parent.controls).forEach((name) => {
-        if (control === parent.controls[name]) {
-          path += name + '.';
-          control = parent;
-        }
-      });
-      parent = parent.parent;
-    }
-
-    path = path.substr(0, (path.length - 1));
-    var dotted = path.indexOf('.');
-
-    if (dotted != -1) {
-      var orderedArray: string[] = path.split('.');
-      orderedArray = orderedArray.reverse();
-      path = '';
-      for (let i = 0; i < orderedArray.length; i++) {
-        path += orderedArray[i] + '.';
-      }
-      path = path.substr(0, (path.length - 1));
-    }
-
-    return path;
-  }
-
-  formErrors: Object = {
-    'firstName': [],
-    'lastName': [],
-    'email': [],
-    'plainPassword.first': [],
-    'plainPassword.second': [],
-  };
-
-  validationMessages: Object = {
-    'response_error': '',
-    'required': 'This field is required.',
-    'minlength': 'This field must be at least 4 characters long.',
-    'maxlength': 'This field cannot be more than 24 characters long.',
-    'passwordsEqual': 'Password mismatch.',
-    'isEmail': 'Email invalid.'
-  };
 }
-
-
-/*
- handleError(control: any) {
-
- var _errors: Object[] = [];
- for (let error in control.errors) {
- _errors.push(this.validationMessages[error]);
- var pathFormError = this.getPathFormError(control);
- this.formErrors[(pathFormError)] = error;
- }
-
- for (var child in control.controls) {
- if (control.controls[child] instanceof AbstractControl && control.controls[child].dirty && !control.controls[child].valid) {
- var childErrors = this.handleError(control.controls[child]);
- if (childErrors) {
- var t: Object[] = [];
- t[child] = childErrors;
- _errors.push(t);
- }
- }
- }
-
- return _errors;
- }
- */
