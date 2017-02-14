b6582f88ebc11acacb6c9e85b7be47237dda4966
Neandher Carlos <neandher89@gmail.com>
2017-02-14 15:49:03 -0200

    ...

--- symfony/src/ApiBundle/Form/ChangePasswordType.php
+++ symfony/src/ApiBundle/Form/ChangePasswordType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
+
 class ChangePasswordType extends PlainPasswordType
 {
     /**
